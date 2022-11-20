39485f3ed17d301409a67c44cd2b0d4ce6c3a292
plbinh <plbinh77@gmail.com>
2022-11-20 18:39:23 +0700

    change port and db_uri

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,21 +1,20 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(
-            process.env.DB_URI,
-            // `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@learn-it.kqb6frx.mongodb.net/?retryWrites=true&w=majority`,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            }
-        )
-        console.log('Connected MongoDB')
-    } catch (error) {
-        console.log(error.message)
-        process.exit(1)
-    }
-}
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@learn-it.kqb6frx.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log('Connected MongoDB');
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
